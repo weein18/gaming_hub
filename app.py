4b56189e56c451f44b5c05367c5711fdf07c0623
Arsenii Kovalenko <arseniiko18@gmail.com>
2026-04-26 19:59:26 +0200

    Fix database path error

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ from itsdangerous import URLSafeTimedSerializer
 
 
 
+if not os.path.exists('instance'):
+    os.makedirs('instance')
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'gaming_hub_secret_key_99'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
