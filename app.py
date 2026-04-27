cf52282ae2e14cd6b2b1c7561cc74e51857eee94
Arsenii Kovalenko <arseniiko18@gmail.com>
2026-04-27 11:27:06 +0200

    Final fix for logos register and app

--- app.py
+++ app.py
@@ -267,7 +267,7 @@ def admin_add_match():
             db.session.add(new_match)
             db.session.commit()
             return redirect(url_for('admin_add_match'))
-        return render_template('/admin/add_match.html')
+        return render_template('/admin/add_match.html', key=ADMIN_ACCESS_KEY)
 
 @app.route('/predict/<int:match_id>', methods=['POST'])
 @login_required
