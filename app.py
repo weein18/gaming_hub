36fb076bd19943e9508539ecb33d204744bdb4a6
Arsenii Kovalenko <arseniiko18@gmail.com>
2026-04-27 13:58:06 +0200

    Increase password length

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ if not os.path.exists(UPLOAD_FOLDER):
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(500), nullable=False)
     xp = db.Column(db.Integer, default=0)
     rank = db.Column(db.String(20), default='Silver')
     bio = db.Column(db.String(200), default='No bio yet...')
