bd612f8b11b5590c83fa099986b86872a562483f
Arsenii Kovalenko <arseniiko18@gmail.com>
2026-07-06 15:10:19 +0200

    ssl port

--- app.py
+++ app.py
@@ -44,9 +44,9 @@ app.config.update(
 )
 
 app.config['MAIL_SERVER'] = 'smtp-relay.brevo.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
