98d1922381bc2e0121bcf5039720f7d26ccc4a1e
Arsenii Kovalenko <arseniiko18@gmail.com>
2026-07-06 14:46:19 +0200

    add email

--- app.py
+++ app.py
@@ -43,13 +43,13 @@ app.config.update(
     SESSION_COOKIE_SAMESITE='Lax',
 )
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_SERVER'] = 'smtp-relay.brevo.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME', 'elitehub040@gmail.com')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', 'qhtsdaclltewmwwd')
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
 mail = Mail(app)
 s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
